1f06c9c0982d23a13048928f80cf77beaa9b78ec
Rizzy-Andrew <vasseghiandrew03@icloud.com>
2026-03-30 20:47:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const searchInput = document.getElementById("searchInput");
+
 searchInput.addEventListener("input", function() {
-  console.log("typing...");
+  const query = searchInput.value.toLowerCase();
+  
+  const items = document.querySelectorAll("ol li");
+  
+  items.forEach(function(item) {
+    const name = item.textContent.toLowerCase();
+    
+    if (name.includes(query)) {
+      item.style.display = "flex";  // show it
+    } else {
+      item.style.display = "none";  // hide it
+    }
+  });
 });
